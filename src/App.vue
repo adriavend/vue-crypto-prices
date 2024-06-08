806601c1a10af80611ece1f983b09146eb5db866
Adrian Avend <adrian.avend@gmail.com>
2024-06-07 21:26:42 -0300

    minor changes

--- src/App.vue
+++ src/App.vue
@@ -41,39 +41,41 @@
       <h1>Coin Market</h1>
       <input type="text" class="form-control bg-dark text-light rounded-0 border-o my-4" placeholder="Search Coin"
         @keyup="searchCoin()" v-model="textSearch" />
-
-      <table class="table table-dark">
-        <thead>
-          <tr>
-            <th v-for="title in coinTitles" :key="title">
-              {{ title }}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(coin, index) in filteredCoins" :key="coin.id">
-            <td class="text-muted">
-              {{ index + 1 }}
-            </td>
-            <td>
-              <img :src="coin.image" alt="icon" style="width: 2rem" class="me-2">
-              <span>{{ coin.name }}</span>
-              <span class="ms-2 text-uppercase text-muted">
-                {{ coin.symbol }}
-              </span>
-            </td>
-            <td>
-              $ {{ coin.current_price }}
-            </td>
-            <td :class="[coin.price_change_percentage_24h > 0 ? 'text-success' : 'text-danger']">
-              {{ coin.price_change_percentage_24h }} %
-            </td>
-            <td>
-              $ {{ coin.total_volume.toLocaleString() }}
-            </td>
-          </tr>
-        </tbody>
+      <div class="table-responsive">
+        <table class="table table-dark">
+          <thead>
+            <tr>
+              <th v-for="title in coinTitles" :key="title">
+                {{ title }}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(coin, index) in filteredCoins" :key="coin.id">
+              <td class="text-muted">
+                {{ index + 1 }}
+              </td>
+              <td>
+                <img :src="coin.image" alt="icon" style="width: 2rem" class="me-2">
+                <span>{{ coin.name }}</span>
+                <span class="ms-2 text-uppercase text-muted">
+                  {{ coin.symbol }}
+                </span>
+              </td>
+              <td>
+                $ {{ coin.current_price }}
+              </td>
+              <td :class="[coin.price_change_percentage_24h > 0 ? 'text-success' : 'text-danger']">
+                {{ coin.price_change_percentage_24h }} %
+              </td>
+              <td>
+                $ {{ coin.total_volume.toLocaleString() }}
+              </td>
+            </tr>
+          </tbody>
       </table>
+      </div>
+      
     </div>
   </div>
 </template>
